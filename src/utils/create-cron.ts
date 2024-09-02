2647dbf81a213fa4ff67562608b96b67f233753a
lhonRafaat <lhontaekwondo@gmail.com>
2024-09-02 03:39:59 +0300

    feat: add create cron helper

---
+++ src/utils/create-cron.ts
@@ -0,0 +1,21 @@
+import { CronJob } from "cron";
+import { ScheduleOptions } from "../cron.interface";
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+export function createCron(
+  cronExpression: string,
+  func: () => void,
+  onCompleted?: () => void,
+  options?: ScheduleOptions,
+): CronJob {
+  //   const isValid = this.validate(cronExpression);
+  //   if (!isValid) {
+  //     this.report.error(`Invalid cron expression: ${cronExpression}`);
+  //   }
+
+  const isImmidiate = options?.imidiate ?? true;
+  const job = new CronJob(cronExpression, func, onCompleted, isImmidiate, options.timezone);
+
+  return job;
+}
